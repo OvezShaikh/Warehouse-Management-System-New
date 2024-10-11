2fe0ef6239562194473d75e424cb67890b5ed156
Ovez <ovezshaikh13052001@gmail.com>
2024-10-11 14:49:55 +0530

    Seperated frontend and backend

---
+++ frontend_new/Frontend/src/components/NavBarComponent.jsx
@@ -0,0 +1,165 @@
+import React from "react";
+import {
+  Box,
+  Grid,
+  AppBar,
+  Container,
+  Typography,
+  Paper,
+  IconButton,
+  Avatar,
+  Badge,
+  Menu,
+  MenuItem,
+  Divider,
+  ListItemIcon,
+  Tooltip,
+} from "@mui/material";
+import {
+  NotificationsOutlined,
+  Settings,
+  Logout,
+  AccountCircleOutlined,
+} from "@mui/icons-material";
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+import images from "../constants/images";
+
+
+export default function NavBarComponent() {
+  const [notificationAnchorEl, setNotificationAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const notificationOpen = Boolean(notificationAnchorEl);
+
+  const handleAvatarClicked = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleNotificationClicked = (event) => {
+    setNotificationAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const notificationHandleClose = () => {
+    setNotificationAnchorEl(null);
+  };
+
+  return (
+    <Grid container sx={{ width: '100%' }}>
+      <Grid item xs={12}>
+        <Paper elevation={4}>
+          <AppBar sx={{ padding: 1 }} position="static"> {/* Reduced padding for smaller screens */}
+            <Container maxWidth="xxl">
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                {/* Logo */}
+                <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+                  <img
+                    src={images.UnostarLogo}
+                    alt="Unostar Logo"
+                    style={{
+                      height: 'auto',
+                      width: '100%', // Makes it responsive in terms of width
+                      maxWidth: '250px', // Sets a maximum width for larger screens
+                      maxHeight: '60px', // Maximum height for larger screens
+                      objectFit: 'contain',
+                    }}
+                  />
+                </Link>
+
+                {/* Icons Section */}
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    alignItems: "center",
+                    flexDirection: "row",
+                    gap: { xs: 0, md: 2 }, // Add gap between icons
+                    mt: { xs: 0, md: 0 }, // No margin on mobile
+                  }}
+                >
+                  <IconButton color="inherit" onClick={handleNotificationClicked}>
+                    <Badge variant="dot" color="error" invisible={false}>
+                      <NotificationsOutlined
+                        sx={{ width: { xs: 20, md: 32 }, height: { xs: 20, md: 32 } }} // Reduce size for extra small screens
+                      />
+                    </Badge>
+                  </IconButton>
+
+                  {/* Notification Menu */}
+                  <Menu
+                    open={notificationOpen}
+                    anchorEl={notificationAnchorEl}
+                    onClose={notificationHandleClose}
+                  >
+                    <MenuItem>Notification number 1</MenuItem>
+                    <Divider />
+                    <MenuItem>Notification number 2</MenuItem>
+                    <MenuItem>Notification number 3</MenuItem>
+                  </Menu>
+
+                  <IconButton
+                    onClick={handleAvatarClicked}
+                    size="small"
+                    aria-haspopup="true"
+                  >
+                    <Tooltip title="account settings">
+                      <Avatar sx={{ width: { xs: 20, md: 32 }, height: { xs: 25, md: 32 } }}>Z</Avatar>
+                    </Tooltip>
+                  </IconButton>
+
+                  {/* Username for larger screens */}
+                  <Typography
+                    sx={{
+                      display: { xs: 'none', md: 'block' }, // Hide username on extra small screens
+                    }}
+                    fontFamily={"Inter"}
+                  >
+                    ADMI ZAKARYAE
+                  </Typography>
+                </Box>
+
+                {/* Avatar Menu */}
+                <Menu
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                >
+                  <MenuItem>
+                    <ListItemIcon>
+                      <AccountCircleOutlined fontSize="small" />
+                    </ListItemIcon>
+                    Profile
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                  </MenuItem>
+                </Menu>
+              </Box>
+            </Container>
+          </AppBar>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
---
+++ frontend_new/Frontend/src/constants/images.js
@@ -0,0 +1,12 @@
+import RealTimeInventoryTracking from '../assets/Real-Time-Inventory-Tracking.jpg';
+import OrderManagement from '../assets/order-management.jpg';
+import SupplierManagement from '../assets/Supplier-management.jpg';
+import UnostarLogo from '../assets/UnostarLogo.png';
+
+
+export default {
+    RealTimeInventoryTracking,
+    OrderManagement,
+    SupplierManagement,
+    UnostarLogo,
+}
